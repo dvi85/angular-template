dedc8a54c3dda5ebf7a507e98eb378df53c992af
denishik@tbm.ru <denishik@tbm.ru>
2019-12-25 23:04:59 +0300

    reorganization project

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { AppMaterialModule } from "../material/app.material.module";
-import { PopupMessageComponent } from "../dialog/popup-message/popup-message.component";
+import { AppMaterialModule } from "../shared/material/app.material.module";
+import { PopupMessageComponent } from "../shared/dialog/popup-message/popup-message.component";
 import {LayoutComponent} from "./layout.component";
 
 @NgModule({
