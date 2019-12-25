e66f3fbe753bfadef29b3f8aaa541ba36c1a1fac
Егоров Антон egorov_ad <egorov_ad@tbm.ru>
2019-12-25 13:49:29 +0300

    added server

---
+++ server/search-lessons.route.ts
@@ -0,0 +1,47 @@
+
+
+
+import {Request, Response} from 'express';
+import {USERS} from "./db-data";
+import {setTimeout} from "timers";
+
+
+
+export function searchUsers(req: Request, res: Response) {
+
+    const queryParams = req.query;
+
+    const filter = queryParams.filter || '',
+          sortOrder = queryParams.sortOrder,
+          pageNumber = parseInt(queryParams.pageNumber) || 0,
+          pageSize = parseInt(queryParams.pageSize);
+
+    let lessons = Object.values(USERS).sort((l1, l2) => l1.id - l2.id);
+
+    if (filter) {
+
+       lessons = lessons.filter(
+           user =>
+               filter === undefined ||
+               filter === null ||
+               filter == "" ||
+               user.login.includes(filter) ||
+               user.name.includes(filter) ||
+               user.surname.includes(filter)
+       );
+    }
+
+    if (sortOrder == "desc") {
+        lessons = lessons.reverse();
+    }
+
+    const initialPos = pageNumber * pageSize;
+
+    const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
+
+    setTimeout(() => {
+        res.status(200).json({payload: lessonsPage});
+    },1000);
+
+
+}
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -27,20 +27,4 @@ export class UsersService {
       })
       .pipe(map(res => res["payload"]));
   }
-
-  getUsersList(search: string): Observable<User[]> {
-    return of(
-      Array.from({ length: 256 }, (value, key) => key)
-        .map(n => new User(n, n + "@gmail.com", "Name" + n, "Surname" + n))
-        .filter(
-          user =>
-            search === undefined ||
-            search === null ||
-            search == "" ||
-            user.login.includes(search) ||
-            user.name.includes(search) ||
-            user.surname.includes(search)
-        )
-    );
-  }
 }
