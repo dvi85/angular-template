1884c7dffb4f2b008e8debe0902bb727acedf002
denishik@tbm.ru <denishik@tbm.ru>
2019-12-26 17:15:04 +0300

    Blocked user

--- src/app/feature/users/users.service.ts
+++ src/app/feature/users/users.service.ts
@@ -3,7 +3,7 @@ import {User} from "./components/store/user";
 import {Observable} from "rxjs/internal/Observable";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Pageable} from "./components/store/pageable";
-import {URL_USERS} from "../../app.constants";
+import {AUTH_SERVER_URL, URL_BLOCK_USER, URL_UNBLOCK_USER, URL_USERS} from "../../app.constants";
 
 @Injectable({
   providedIn: "root"
@@ -20,5 +20,13 @@ export class UsersService {
           .set("page", pageNumber.toString())
           .set("size", pageSize.toString())
       });
+  }
+
+  blockUser(user: User): Observable<any> {
+    return  this.http.put(URL_BLOCK_USER(user.id), {});
+  }
+
+  unBlockUser(user: User): Observable<any> {
+    return  this.http.put(URL_UNBLOCK_USER(user.id), {});
   }
 }
