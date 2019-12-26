09b71beda839e485b32039d46b6d96140af0a103
denishik@tbm.ru <denishik@tbm.ru>
2019-12-26 14:28:07 +0300

    Token code improve

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,21 +1,14 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {tap} from 'rxjs/operators';
-import {ACCESS_TOKEN, AUTH_URL, CLIENT_ID, CLIENT_SECRET, EXPIRES_IN, REFRESH_TOKEN} from "../../app.constants";
+import {ACCESS_TOKEN, AUTH_URL} from "../../app.constants";
 import {Router} from "@angular/router";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 export class AuthResponse {
-    // tslint:disable-next-line:variable-name
-    access_token: string;
-    // tslint:disable-next-line:variable-name
-    token_type: string;
-    // tslint:disable-next-line:variable-name
-    refresh_token: string;
-    // tslint:disable-next-line:variable-name
-    expires_in: number;
-    scope: string;
-    organization: string;
+    accessToken: string;
+    tokenType: string;
 }
 
 @Injectable({
@@ -23,16 +16,13 @@ export class AuthResponse {
 })
 export class AuthService {
 
+    private jwtHelper: JwtHelperService = new JwtHelperService();
+
     constructor(private http: HttpClient, private router: Router) {
     }
 
     get token(): string {
-        let expDate
-        if (localStorage.getItem(EXPIRES_IN )) {
-            expDate = new Date(localStorage.getItem(EXPIRES_IN));
-        }
-        console.log('expDate EXPIRES_IN', expDate)
-        if (!expDate || new Date() > expDate) {
+        if (this.jwtHelper.isTokenExpired(localStorage.getItem(ACCESS_TOKEN))) {
             this.logout();
             return null;
         }
@@ -40,18 +30,7 @@ export class AuthService {
     }
 
     login(username: string, password: string): Observable<any> {
-        const params = new URLSearchParams();
-        params.append('username', username);
-        params.append('password', password);
-        params.append('grant_type', 'password');
-
-        const httpOptions = {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/x-www-form-urlencoded',
-                Authorization: 'Basic ' + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`),
-            }),
-        };
-        return this.http.post<any>(AUTH_URL, params.toString(), httpOptions).pipe(
+        return this.http.post<any>(AUTH_URL, {email: username, password: password}).pipe(
             tap(response => this.setToken(response))
         );
     }
@@ -68,11 +47,10 @@ export class AuthService {
 
     private setToken(response: AuthResponse | null) {
         if (response) {
-            const expDate = new Date(new Date().getTime() + +response.expires_in * 1000);
-            console.log('expDate tokean', expDate);
-            localStorage.setItem(ACCESS_TOKEN, response.access_token);
-            localStorage.setItem(REFRESH_TOKEN, response.refresh_token);
-            localStorage.setItem(EXPIRES_IN, expDate.toString());
+            console.log(this.jwtHelper.decodeToken(response.accessToken));
+            console.log(this.jwtHelper.getTokenExpirationDate(response.accessToken));
+            console.log(this.jwtHelper.isTokenExpired(response.accessToken));
+            localStorage.setItem(ACCESS_TOKEN, response.accessToken);
         } else {
             localStorage.clear();
         }
