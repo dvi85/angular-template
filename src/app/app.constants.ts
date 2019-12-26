bca144a0462c84456d8222ac6d558b8cb3f440ba
denishik@tbm.ru <denishik@tbm.ru>
2019-12-26 14:28:07 +0300

    Token code improve

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,8 +1,6 @@
-export const CLIENT_ID = 'tbmBonusClientPassword';
-export const CLIENT_SECRET = 'tbm_bonus_2018@tbm.ru';
-export const AUTH_URL = `https://oauth.tbm.ru/server/oauth/token`;
-export const ACCESS_TOKEN = `access_token`;
-export const REFRESH_TOKEN = `refresh_token`;
+import {environment} from "../environments/environment";
+export const AUTH_URL =  `${environment.authServerUrl}/api/login`;
+export const ACCESS_TOKEN = `accessToken`;
 export const EXPIRES_IN = `expires_in`;
 
 
