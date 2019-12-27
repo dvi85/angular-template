1ae5c7da105abd3a54b1af2d14009a3c986d1525
Егоров Антон egorov_ad <egorov_ad@tbm.ru>
2019-12-27 15:55:23 +0300

    403 page styling

--- src/app/feature/users/pages/forbidden403/forbidden403.component.ts
+++ src/app/feature/users/pages/forbidden403/forbidden403.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-forbidden403',
   templateUrl: './forbidden403.component.html',
-  styleUrls: ['./forbidden403.component.css']
+  styleUrls: ['./forbidden403.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class Forbidden403Component implements OnInit {
 
