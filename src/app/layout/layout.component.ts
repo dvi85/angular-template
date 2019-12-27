45cdaee00bf3d7395c0ff721525653c7b9102e50
denishik@tbm.ru <denishik@tbm.ru>
2019-12-27 12:55:14 +0300

    username

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,7 +1,9 @@
-import {ChangeDetectorRef, Component, Input, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 import {MediaMatcher} from "@angular/cdk/layout";
 import {AuthService} from "../core/auth/auth.service";
-import {User} from "../feature/users/components/store/user";
+import {UsersService} from "../feature/users/users.service";
+import {Observable} from "rxjs";
+import {pluck} from "rxjs/operators";
 
 @Component({
   selector: 'app-container',
@@ -9,13 +11,13 @@ import {User} from "../feature/users/components/store/user";
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnDestroy {
-    @Input() currentUser: User;
+    userName$: Observable<string> = this.userService.getCurrentUser().pipe(pluck('name'));
     mobileQuery: MediaQueryList;
 
 
     private _mobileQueryListener: () => void;
 
-    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private authService:AuthService) {
+    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private authService:AuthService, private userService: UsersService) {
         this.mobileQuery = media.matchMedia('(max-width: 600px)');
         this._mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener(this._mobileQueryListener);
